47c66ac886e0af22ef694f0dc3f0f7195463b5df
Dull Bananas <dull.bananas0@gmail.com>
2021-02-23 04:15:51 +0000

    Add TextDisplay variant of Model

--- src/Editsc.elm
+++ src/Editsc.elm
@@ -3,11 +3,22 @@ module Editsc exposing (main)
 import Browser
 
 
-main : Program () () Never
+main : Program () Model Never
 main =
     Browser.document
-        { init = always ((), Cmd.none)
+        { init = init
         , view = always { title = "EditSC", body = [] }
         , update = never
         , subscriptions = always Sub.none
         }
+
+
+type Model
+    = TextDisplay String
+
+
+init : () -> (Model, Cmd Never)
+init flags =
+    ( TextDisplay ""
+    , Cmd.none
+    )
